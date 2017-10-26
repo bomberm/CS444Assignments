70dcefab72ecdb83be8b0b4bf0ba7e5b41ca4bfc
Mary Bomber <bomberm@os2.engr.oregonstate.edu>
2017-10-26 13:39:30 -0700

    Removing files that have been moved

--- Code/functions.h
+++
@@ -1,9 +0,0 @@
-
-void init_genrand(unsigned long seed);
-unsigned long genrand_int32();
-long genrand_int31();
-double genrand_real1();
-double genrand_real2();
-double genrand_real3();
-double genrand_res53();
-
