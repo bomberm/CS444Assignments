289be37e3f16bf33d7f6f915aff00fa9678ce742
Chris Bakkom <chris.bakkom@gmail.com>
2017-10-26 20:45:01 -0700

    Added core structures
    Adding mutex lock, the semaphore
    Added up and down for exclusive semaphore
    Added pflags for what the philosophers are doing

--- Concurrency2/Code/diningPhilosophers.cpp
+++ Concurrency2/Code/diningPhilosophers.cpp
@@ -1,26 +1,40 @@
 #include "diningPhilosophers.h"
+#include <thread>
+#include <mutex>
+
+std::mutex sem_lock;
+std::vector<pflag> phil_flags[5];
+
 
 int main(void)
 {
 
-  std::vector<Fork> forks;
-  std::vector<Philosopher> philosophers = loadPhilosophers();
-  
-  assignForks(forks, philosophers);
+	std::vector<Fork> forks;
+	std::vector<Philosopher> philosophers = loadPhilosophers();
 
-  beginEating(forks);
+	assignForks(forks, philosophers);
 
-  while(1);
+	beginEating(forks);
+
+	while (1);
 }
 
 Philosopher::Philosopher(string myName, which hand)
 {
-  name = myName;
-  handedness = hand;
+	name = myName;
+	handedness = hand;
 };
+
+void down() {
+	sem_lock.lock;
+}
+
+void up() {
+	sem_lock.unlock();
+}
 
 std::vector loadPhilosophers()
 {
 
-  std::vector<Philosopher> philosophers;
-  
+	std::vector<Philosopher> philosophers;
+
